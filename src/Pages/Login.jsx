b06830c591826efa60701f3634a80b50087d5074
srinidhisrinivasan.06 <167874294+srinidhisrinivasan06@users.noreply.github.com>
2025-12-19 15:18:06 +0530

    s

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
     e.preventDefault();
     
     try {
-      const response = await fetch('http://localhost:5000/api/auth/login', {
+      const response = await fetch('https://collab-backend-wjcd.onrender.com/api/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password })
